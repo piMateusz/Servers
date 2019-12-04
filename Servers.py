f2347411677c4139d26ef615a952fe1ade31b28b
piMateusz <pilecki.mat@gmail.com>
2019-12-04 22:06:06 +0100

    little changes

--- Servers.py
+++ Servers.py
@@ -5,12 +5,11 @@ from typing import Optional, List, Dict
 from abc import ABC, abstractmethod
 import re
 
+
 class Product:
     def __init__(self, name: str, price: float):
         self.name = name
         self.price = price
-
-    # TO_DO
 
     def __hash__(self):
         return hash((self.name, self.price))
@@ -25,17 +24,20 @@ class TooManyProductsFoundError:
 
 class Server(ABC):
 
-    def get_entries(self, n_letters: int = 1)-> List[float]:
-        price_list: List[float] = filtrate(n_letters)
-
-        # TO DO - sort returned list
+    def get_entries(self, n_letters: int = 1) -> List[float]:
+        pattern = '[a - z]{n}[0-9]{2-3}'
+        try:
+            price_list: List[float] = filtrate(pattern)
+            if price_list is None:      # None if too many products
+                raise ValueError()      # TO DO: replace it with appropriate error
+            else:
+                return price_list       # TO DO - sort returned list here
+        except ValueError():
+            print("Too many products in server !")
 
     @abstractmethod
     def filtrate(self, n_letters: int = 1):
-        pass
-
-#   (3) możliwość odwołania się do metody
-#  `get_entries(self, n_letters)` zwracającą listę produktów spełniających kryterium wyszukiwania
+        raise NotImplementedError()
 
 
 class ListServer(Server):
@@ -45,20 +47,20 @@ class ListServer(Server):
     def __init__(self, products: List[Product]):
         self.products = products
 
-    def filtrate(self, n_letters: int = 1):
+    def filtrate(self, pattern: str):
         price_list: List[float] = []
         for product_index in range(len(self.products)):
             if product_index == n_max_returned_entries - 1:
-                break
-            if re.match('[a - z]{n}[0-9]{2-3}', self.products[product_index].name):
+                return None
+            if re.match(pattern, self.products[product_index].name):
                 price_list.append(product.name)
 
         return price_list
 
+
 class MapServer:
-    def __init__(self, product_dict: Dict[Product.name,Product]):
-        self.product_dict= product_dict
-   
+    def __init__(self, product_dict: Dict[Product.name,Product]):       #konstruktor ma brac jako argument liste produktow tak jak w ListServer (products: List[Product])
+        self.product_dict= product_dict                                 #i z niej inicjalizowac slownik (kluczem jest nazwa produktu, wartością – obiekt reprezentujący produkt)
 
 
 class Client:
